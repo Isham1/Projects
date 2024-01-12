191ca82f929e30e1a653fea6a3ad227744ac8312
Isham1 <isham.ydv@gmail.com>
2024-01-12 15:02:34 -0800

    Updated description verbage

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -7,17 +7,17 @@ def bmi (weight, height_inches):
     print('Your BMI is: ', bmi)
     if bmi > 0:
         if bmi < 18.5:
-            print ("You are under-weight with minimal health risk")
+            print ("You are under-weight with minimal health risk.")
         elif bmi >= 18.8 and bmi <= 24.9:
-            print ("You have normal weight with minimal health risk")
+            print ("You have normal weight with minimal health risk.")
         elif bmi >= 25 and bmi <= 29.9:
-            print ("You are over-weight with increased health risk")
+            print ("You are over-weight with increased health risk.")
         elif bmi >= 30 and bmi <= 34.9:
-            print ("You are obese with minimal high risk")
+            print ("You are obese with minimal high risk.")
         elif bmi >= 35 and bmi <= 39.9:
-            print ("You are severly-obese with very high health risk")
+            print ("You are severly-obese with very high health risk.")
         elif 40 <= bmi:
-            print ("You are morbidly-obese with extremely high health risk")
+            print ("You are morbidly-obese with extremely high health risk.")
     else:
         print('Your bmi is out of range!!')
 
